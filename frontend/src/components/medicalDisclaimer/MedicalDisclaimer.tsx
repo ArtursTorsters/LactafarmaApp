224ddbf74c11c553b5e7d271f071de06abcae48b
Arturs Torsters <arrturs.torsters@gmail.com>
2025-09-15 23:07:42 +0300

    legal and css update

--- frontend/src/components/medicalDisclaimer/MedicalDisclaimer.tsx
+++ frontend/src/components/medicalDisclaimer/MedicalDisclaimer.tsx
@@ -1,17 +1,59 @@
-import React from 'react';
-import { View, Text} from 'react-native';
-import {  medicalDisclaimerStyles } from '../../styles/styles';
+import React from "react";
+import { View, Text, Linking } from "react-native";
+import { globalStyles, medicalDisclaimerStyles } from "../../styles/styles";
 
 export const MedicalDisclaimer: React.FC = () => {
+  const openPolicy = () => {
+    Linking.openURL("https://arturstorsters.github.io/privacy-policy");
+  };
+  const openTerms = () => {
+    Linking.openURL("https://arturstorsters.github.io/terms-of-service");
+  };
+
   return (
     <View style={medicalDisclaimerStyles.container}>
       <View style={medicalDisclaimerStyles.header}>
-        <Text style={medicalDisclaimerStyles.title}>Important Medical Disclaimer</Text>
+        <Text style={medicalDisclaimerStyles.title}>
+          Important Medical Disclaimer
+        </Text>
       </View>
       <Text style={medicalDisclaimerStyles.text}>
-         ⚠️ This information does not substitute the advice of a healthcare professional.
-        Always consult your doctor before making medication decisions during breastfeeding.
+        ⚠️ This information does not substitute the advice of a healthcare
+        professional. Always consult your doctor before making medication
+        decisions during breastfeeding.
+
       </Text>
+
+      <View
+       style={[
+        {
+          justifyContent: 'space-between',
+          flexDirection: 'row',
+        },
+      ]}>
+
+          <Text
+          style={[globalStyles.bodyText, globalStyles.link]}
+          onPress={openPolicy}
+          accessibilityRole="button"
+          accessibilityLabel="Read our policy"
+        >
+          → Read our policy
+        </Text>
+        <Text
+          style={[globalStyles.bodyText, globalStyles.link]}
+          onPress={openTerms}
+          accessibilityRole="button"
+          accessibilityLabel="Read our terms"
+        >
+          → Read our terms
+        </Text>
+
+
+
+
+      </View>
+
     </View>
   );
 };
